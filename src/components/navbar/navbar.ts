4cbcab72751645343c0fbaf91952623998548f7c
juan-camilo-11 <thekmilo84@gmail.com>
2024-05-10 19:57:08 -0500

    Nueva version de componente Navbar

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,24 +1,17 @@
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { pasterSelectColor, selectTextColor } from "../../utils/componentsTypes";
-import { SizeComponent, TypeMessage, defatulText } from '../ constants/ constants';
-
 
 @customElement('pure-navbar')
 export class Navbar extends LitElement {
 
     @property()
-    navItems = [
-        { text: 'Inicio', url: '#home', logo: html` <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#AAA" d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>` },
-        { text: 'Acerca de', url: '#about', logo: html` <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#AAA" d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>` },
-        { text: 'Servicios', url: '#services', logo: html` <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#AAA" d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>` },
-        { text: 'Contacto', url: '#contact', logo: html` <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#AAA" d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>` }
-    ];
-    @property()
-    logoUrl?: string = "";
+    navItems: string = '';
 
     @property()
-    tittle: string = "Lego"
+    logoUrl?: string = "https://upload.wikimedia.org/wikipedia/commons/c/ce/Coca-Cola_logo.svg";
+
+    @property()
+    logoText?: string = "";
 
     @property()
     private isActive?: boolean = false;
@@ -26,30 +19,29 @@ export class Navbar extends LitElement {
     static styles = css`
     nav{
         position: fixed;
-        top: .5rem;
-        left: .5rem;
-        width: calc(4.5rem - 1rem);
-        height: calc(100% - 1rem);
+        top: 0;
+        left: -100%;
+        width: 50%;
+        height: 100%;
         background-color: #F4F6F6;
-        border-radius: .5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        transition: 0.3s ease;
+        transition: 0.5s ease;
+    }
+    nav.active{
+        display:flex;
+        left: 0;
+        transition: 0.5s ease;
     }
     ul{
-        width: calc(100% -1rem);
+        width: 100%;
+        padding: 0;
         list-style: none;
-        padding: 0 .5rem;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
     }
     li{
-        width: calc(100% - .6rem);
-        padding: .5rem .5rem;
+        width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -57,69 +49,42 @@ export class Navbar extends LitElement {
     }
     li:hover{
         color: #FFF;
-        border-radius: .5rem;
+        border-right: solid 1px #5e5e5e;
         background-color: #EAEDED;
         transition: 0.3s ease;
     }
-    li:hover > a {
+    li:hover a {
         color: #000;
-    }
-    li:hover path {
-        fill: black;
-        transition: 0.3s ease;
     }
     a{
         width: calc(100% - 1rem);
+        height: calc(100% - 1.6rem);
+        padding: .8rem 0rem;
         color: #AAA;
         text-decoration: none;
         margin-left: .5rem;
     }
     li.active{
+        border-right: solid 1px #5e5e5e;
         background-color: #EAEDED;
     }
     li.active a{
-        color: #FFF;
+        color: #000;
     }
     img{
-        width: 30px;
-        heigth: 30px;
+        width: 4rem;
+        height: auto;
         margin: 1rem 0;
     }
-    li a{
-        display: none;
-        transition: 0.3s ease;
-    }
-    nav.active li a{
-        display: inline-block;
-        transition: 0.5s ease;
-    }
-    nav.active li{
-        justify-content: flex-start;
-    }
-    div{
+    .container{
         display:flex;
-        justify-content: space-between;
+        flex-direction: column;
+        justify-content: flex-start;
         align-items: center;
+        width: 100%;
+        height: 100%;
     }
-    div p{
-        display:none;
-    }
-    nav.active div p{
-        display: inline-block;
-        margin-left: .5rem;
-        font-size: 20px;
-        letter-spacing: 1px;
-        font-weight: 500;
-    }
-    .arrow {
-        width: .5rem;
-        height: .5rem;
-        background-color: none;
-        transform: rotate(45deg);
-        border-top: 2px solid #AAA;
-        border-right: 2px solid #AAA;
-    }
-    button{
+    .menu{
         display: flex;
         justify-content: center;
         align-items: center;
@@ -127,97 +92,163 @@ export class Navbar extends LitElement {
         height: auto;
         border: none;
         background-color: #F4F6F6;
-        margin-bottom: 1rem;
+        cursor: pointer;
     }
-    nav.active .arrow{
-        border-top: none;
-        border-right: none;
-        border-bottom: 2px solid #AAA;
-        border-left: 2px solid #AAA;
+    .menu-icon {
+        width: 25px;
+        height: 2px;
+        background-color: #AAA;
+        position: relative;
+        margin: 20px auto;
+        transition: background-color .3s ease;
     }
-    button:hover .arrow{
-        border-color:#000;
-        transition: 0.3s ease;
+    
+    .menu-icon::before,
+    .menu-icon::after {
+        content: '';
+        width: 25px;
+        height: 2px;
+        background-color: #AAA;
+        position: absolute;
+        transition: background-color .3s ease;
     }
-    @media screen and (max-width: 768px) {
-        nav{
-            width: calc(3.5rem - .6rem);
-            left: .3rem;
-        }
-        nav.active{
-            transition: 0.3s ease;
-            width: calc(65% - 1rem);
-        }
+    
+    .menu-icon::before {
+        top: -8px;
+        left: 0;
     }
-    @media screen and (min-width: 769px) {
-        nav.active{
-            transition: 0.3s ease;
-            width: calc(35% - 1rem);
-        }
+    
+    .menu-icon::after {
+        top: 8px;
+        left: 0;
+    }
+    .menu:hover .menu-icon,
+    .menu:hover .menu-icon::before,
+    .menu:hover .menu-icon::after
+    {
+        background-color: #000;
+        transition: background-color .3s ease;
+    }
+    .d-flex {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap:.4rem;
+    }
+    .container > div{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
     @media screen and (max-width: 320px) {
-        nav{
-            width: calc(3.5rem - .6rem);
-            left: .3rem;
-        }
         nav.active{
             transition: 0.3s ease;
             width: calc(70% - 1rem);
         }
     }
-
+    
     @media screen and (min-width: 321px) and (max-width: 480px) {
         nav.active{
             transition: 0.3s ease;
             width: calc(65% - 1rem);
         }
     }
-
+    
     @media screen and (min-width: 481px) and (max-width: 768px) {
         nav.active{
             transition: 0.3s ease;
-            width: calc(60% - 1rem);
+            width: calc(40% - 1rem);
         }
     }
-
+    
     @media screen and (min-width: 769px) and (max-width: 1024px) {
         nav.active{
             transition: 0.3s ease;
-            width: calc(50% - 1rem);
+            width: calc(30% - 1rem);
         }
     }
-
+    
     @media screen and (min-width: 1025px) {
         nav.active{
             transition: 0.3s ease;
-            width: calc(25% - 1rem);
+            width: calc(20% - 1rem);
         }
     }
-
+    
+    
+    .close-icon {
+        width: .8ren;
+        height: .8rem;
+        position: relative;
+        padding: .3rem .5rem;
+    }
+    
+    .close-icon::before,
+    .close-icon::after {
+        content: '';
+        position: absolute;
+        width: 2px;
+        height: 100%;
+        background-color: #AAA;
+    }
+    .btn-close:hover .close-icon::before,
+    .btn-close:hover .close-icon::after {
+        background-color: #000;
+        transition: 0.3s ease;
+    }
+    
+    .close-icon::before {
+        transform: rotate(45deg);
+    }
+    
+    .close-icon::after {
+        transform: rotate(-45deg);
+    }
+    .btn-close{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: auto;
+        height: auto;
+        border: none;
+        background-color: #F4F6F6;
+        cursor: pointer;
+        margin-right: .5rem;
+    }
   `;
 
     private handleClick() {
         this.isActive = !this.isActive;
+        console.log(this.isActive)
     }
 
 
     render() {
+        const items: { text: string, url: string, active: string }[] = JSON.parse(this.navItems);
         return html`
+        <button @click="${this.handleClick}" class="menu"><div class="menu-icon"></div></button>
         <nav role="navigation" aria-label="Main" class="${this.isActive ? "active" : ""}">
-        <div>
-            ${this.logoUrl ? html`<img src="${this.logoUrl}" alt="Logo">` : ''}
-            <p>${this.tittle}</p>
+        <div class="container">
+            <div>
+                <div class="d-flex" style="margin-left: .5rem;">
+                    ${this.logoUrl ? html`<img src="${this.logoUrl}" alt="Logo">` : ''}
+                    <p>${this.logoText}</p>
+                </div>
+                <button @click="${this.handleClick}" class="${!this.isActive ? "active" : ""} btn-close">
+                    <div class="close-icon d-flex"></div>
+                </button>
+            </div>
+            <ul>
+                ${items.map(item => 
+                    html`
+                    <li class="${item.active === "true" ? "active" : ""}">
+                        <a href="${item.url}" @click="${this.handleClick}">${item.text}</a>
+                    </li>
+                    `
+                )}
+            </ul>
         </div>
-        <ul>
-        ${this.navItems.map(item =>
-            html`
-            <li>
-            <span>${item.logo}</span>
-                <a href="${item.url}">${item.text}</a>
-            </li>
-            `)}
-        </ul>
-            <button @click="${this.handleClick}"><span class="arrow"></span></button>
         </nav>
         `;
     }
